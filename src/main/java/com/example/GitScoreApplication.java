88ccad1e1a223567d80db5a0eb7249d8786cf378
javalover-design <72918714+javalover-design@users.noreply.github.com>
2022-03-30 12:38:38 +0000

    git
    The first git file

---
+++ src/main/java/com/example/GitScoreApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lambda
+ */
+@SpringBootApplication
+public class GitScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitScoreApplication.class, args);
+    }
+
+}
